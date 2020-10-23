fb59c95874ade9d28edda1281accdcc4f0ae2f98
Afzal Zoubeiri <afzalzoubeiri@gmail.com>
2020-10-23 11:04:41 +0200

    Update tp.js

--- js/tp.js
+++ js/tp.js
@@ -1,16 +1,33 @@
  const form = document.querySelector("form");
 
-    form.addEventListener("submit", function(){
-        nom = form.elements.name.value;
-        prenom = form.elements.pn.value;
+function addElement (contenuForm) {
+    // crée un nouvel élément div
+    newDiv = document.createElement("div");
+    // et lui donne un peu de contenu
+    newContent = document.createTextNode(contenuForm);
+    // ajoute le nœud texte au nouveau div créé
+    newDiv.appendChild(newContent);
+    
+    // ajoute le nouvel élément créé et son contenu dans le DOM
+    currentDiv = document.getElementById('div1');
+    document.body.insertBefore(newDiv, currentDiv);
+}
 
-        if (nom == '' || prenom == ''){
-            alert("Veuillez remplir le formulaire");
-        }
+form.addEventListener("submit", function(e){
+    nom = form.elements.name.value;
+    prenom = form.elements.pn.value;
 
-        else{
-            alert("votre nom est: " + nom + " votre prenom est: " + prenom)
-        }
+    if (nom == '' || prenom == ''){
+        contenuForm = "Veuillez remplir le formulaire"
+        addElement(contenuForm);
+    }
 
+    else{
+        
+        contenuForm = "votre nom est: " + nom + " votre prenom est: " + prenom
+        addElement(contenuForm);
+        
+    }
+    e.preventDefault();
 
-    })
+})
